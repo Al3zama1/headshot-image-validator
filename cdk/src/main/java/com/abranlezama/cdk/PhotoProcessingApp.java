5f5558bf98e86c46cc8f55d9f672e94214ed0c60
Abran Lezama <lezama.abran@gmail.com>
2023-05-08 01:19:21 -0700

    Created s3 bucket and photo processing lambda
    - Created s3 bucket that triggers lambda function on PUT event.
    - Lambda function calls rekognition api to do face recognition on passed
      image.:

---
+++ cdk/src/main/java/com/abranlezama/cdk/PhotoProcessingApp.java
@@ -0,0 +1,43 @@
+package com.abranlezama.cdk;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.services.iam.Effect;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+
+import java.util.List;
+
+public class PhotoProcessingApp {
+    public static void main(String[] args) {
+        App app = new App();
+
+       PhotoProcessingStack photoProcessingStack = new PhotoProcessingStack(app, "PhotoProcessingStack");
+
+       photoProcessingStack.triggerLambdaOnImageUpload(
+               photoProcessingStack.photoProcessingLambda,
+               photoProcessingStack.bucket
+       );
+
+       photoProcessingStack.photoProcessingLambda.addToRolePolicy(
+               PolicyStatement.Builder.create()
+                       .sid("photoProcessingLambdaGetS3Object")
+                       .effect(Effect.ALLOW)
+                       .actions(List.of("s3:GetObject"))
+                       .resources(List.of(
+                               "arn:aws:s3:::headshot-verification-images/*"
+                       ))
+                       .build()
+       );
+       photoProcessingStack.photoProcessingLambda.addToRolePolicy(
+               PolicyStatement.Builder.create()
+                       .sid("photoProcessingLambdaDetectFaces")
+                       .effect(Effect.ALLOW)
+                       .actions(List.of(
+                               "rekognition:DetectFaces"
+                       ))
+                       .resources(List.of("*"))
+                       .build()
+       );
+
+        app.synth();
+    }
+}
---
+++ cdk/src/main/java/com/abranlezama/cdk/PhotoProcessingStack.java
@@ -0,0 +1,55 @@
+package com.abranlezama.cdk;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.lambda.Architecture;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.eventsources.S3EventSourceProps;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.EventType;
+import software.constructs.Construct;
+import software.amazon.awscdk.services.lambda.eventsources.S3EventSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoProcessingStack extends Stack {
+    public PhotoProcessingStack(final Construct scope, final String id) {
+        super(scope, id);
+
+
+
+    }
+
+    // Create S3 Bucket
+    Bucket bucket = Bucket.Builder
+            .create(this,  "recognitionImageBucket")
+            .bucketName("headshot-verification-images")
+            .build();
+
+
+
+    Function photoProcessingLambda = Function.Builder
+            .create(this, "photoProcessingLambda")
+            .runtime(Runtime.JAVA_17)
+            .architecture(Architecture.X86_64)
+            .timeout(Duration.seconds(15))
+            .memorySize(512)
+            .handler("com.abranlezama.PhotoProcessingLambda::handleRequest")
+            .code(Code.fromAsset("../lambdas/photo-validation/target/photo-validation-1.0-SNAPSHOT.jar"))
+            .build();
+
+    void triggerLambdaOnImageUpload(Function function, Bucket bucket) {
+        List<EventType> lambdaS3EventSource = new ArrayList<>();
+        lambdaS3EventSource.add(EventType.OBJECT_CREATED_PUT);
+
+        function.addEventSource(
+                new S3EventSource(bucket, S3EventSourceProps.builder()
+                        .events(lambdaS3EventSource)
+                        .build())
+        );
+    }
+
+}
